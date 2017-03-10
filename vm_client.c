7e18d0d6d4a76495c4334b8b3e82ef60da597b83
Andrew Courtney <acourtn2@linux-a2.ews.illinois.edu>
2017-03-10 14:08:51 -0600

    test vm_client

--- vm_client.c
+++ vm_client.c
@@ -10,6 +10,21 @@
 int main(int argc, char **argv)
 {
   int socket_fd = setUpClient("sp17-cs241-005.cs.illinois.edu", "9001");
+  char myChk = (char)(0x09);
+  char myEOF = (char)(0x05);
+  char* toSend = RECEIVEME;
+  char headedMessage[strlen(toSend) + 1 + 1];
+  headedMessage[0] = myChk;
+  strcat(headedMessage+1, toSend);
+  fprintf(stdout, "sending: %s\n", headedMessage);
+  write(socket_fd, headedMessage, strlen(headedMessage));
+  sleep(1);
+  headedMessage[0] = myEOF;
+  headedMessage[1] = '\0';
+  fprintf(stdout, "now sending EOF: %s\n", headedMessage);
+  write(socket_fd, headedMessage, strlen(headedMessage));
+  while(1){};
 //  getBinaryFile(socket_fd, "./gotHi.out");
   return cleanUpClient(socket_fd);
+  //return 1;
 }
