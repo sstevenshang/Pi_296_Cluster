94a031b2eba13f577ab39b059683381c727bd757
Steven Shang <stevenshang@wirelessprv-10-194-236-199.near.illinois.edu>
2017-03-14 18:10:49 -0500

    added heartbeat

---
+++ heartbeat.c
@@ -0,0 +1,119 @@
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/types.h>
+
+#define BUFSIZE 1024
+#define SERVER_PORT 1153
+
+int setUpUDPClient() {
+
+	int socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	if (socket_fd < 0) {
+		perror("FAILED: unable to create socket");
+		return -1;
+	}
+
+	return socket_fd;
+}
+
+int setUpUDPServer() {
+
+	int socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (socket_fd < 0) {
+		perror("FAILED: unable to create socket");
+		return -1;
+	}
+
+	struct sockaddr_in serverAddr;
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(SERVER_PORT);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	memset(serverAddr.sin_zero, 0, sizeof(serverAddr.sin_zero));
+
+	int status = bind(socket_fd, (struct  sockaddr*) &serverAddr, sizeof(serverAddr));
+	if (status < 0) {
+		perror("FAILED: unable to bind socket");
+		return -1;
+	}
+
+	return socket_fd;
+}
+
+void cleanupUDPSocket(int socket_fd) {
+	close(socket_fd);
+}
+
+void heartbeat(char* destinationAddr, char* destinationPort, int* alive) {
+	int socket_fd = setUpUDPClient();
+	int status;
+	while (*alive) {
+		while (sendHeartbeat(socket_fd, destinationAddr, destinationPort) == -1) {
+			printf("Failed: failed to send heartbeat");
+		}
+		sleep(3);
+	}
+}
+
+int sendHeartbeat(int socket_fd, char* destinationAddr, char* destinationPort) {
+
+	char* message = "BEAT";
+	size_t length = strlen(message) + 1;
+
+	struct sockaddr_in serverAddr;
+
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = inet_addr(destinationAddr);
+	serverAddr.sin_port = inet_addr(destinationPort);
+	memset(serverAddr.sin_zero, 0, sizeof(serverAddr.sin_zero));
+
+	socklen_t dest_len = sizeof(serverAddr);
+
+	int status = sendto(socket_fd, message, length, 0, (struct sockaddr*) &serverAddr, dest_len);
+
+	if (status < 0) {
+		perror("FAILED: unable to send message to server");
+		return -1;
+	}
+
+	return 0;
+}
+
+void listenToHeartbeat(int socket_fd, int* stethoscope) {
+
+	unsigned char buffer[BUFSIZE];
+	struct sockaddr_in clientAddr;
+	socklen_t addrlen = sizeof(clientAddr);
+	int byte_received = 0;
+
+	while(*stethoscope) {
+
+		byte_received = recvfrom(socket_fd, buffer, BUFSIZE, 0, (struct sockaddr*)&clientAddr, &addrlen)
+		if (byte_received < 0) {
+			perror("FAILED: failed to receive from client");
+		} else {
+			char* beat_addr = inet_ntoa(clientAddr.sin_addr);
+			char* beat_port = inet_ntoa(clientAddr.sin_port);
+			
+			reportHeartbeat(beat_addr, beat_port);
+			printf("SUCCESS: received \"%s\" from %s:%s\n", buffer, beat_addr, beat_port);
+		}
+	}
+}
+
+void reportHeartbeat(char* beat_addr, char* beat_port) {
+	/* keep track of num of beat/skip balance
+	   kill node if skip imbalanced
+	   revive node if beat imbalanced*/
+
+	updateNodeStatus();
+}
+
+
+
+
+
+
+
