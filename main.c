abe4943d4ae6d49fdbac3f474df2c91a73f5e9a4
Eryk <eryk@helenowski.com>
2017-03-16 21:55:44 -0500

    Revert "Fix compile errors in main"
    This reverts commit aeed697cdd4688ee0ae431229d9a09246ad36dab.

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include "node.h"
-#include "server.h"
+// #include "server.h"
 #include "master.h"
 
 #include <string.h>
@@ -7,11 +7,9 @@
 
 int main(int argc, char const *argv[])
 {
-	node* this_node;
-	// node_id* this_node;
+	node_id* this_node;
 	this_node = construct_nodes();
-	if (this_node == cur_master) {
-	// if (this_node->master == IS_MASTER) {
+	if (this_node->master == IS_MASTER) {
 		master_main(this_node);
 	} else {
 		server_main(this_node);
