058c06c6f69bc5fd07c4af8db3c4ada6728ef075
Bennett Ip <ngai0408@gmail.com>
2017-04-06 20:26:03 -0500

    Change interface.c

--- interface.c
+++ interface.c
@@ -8,32 +8,34 @@ void print_usage();
 
 int main(int argc, char const *argv[]) {
 	printf("1234\n");
-	wait_for_input(argv);
+	wait_for_input();
 	printf("2345\n");
 	return 0;
 }
 
-void print_usage(char const *argv[]) {
-	printf("usage: %s [executable]\n", argv[0]);
-}
-
-int wait_for_input(char const *argv[]) {
+int wait_for_input() {
 
 	char* buffer = NULL;
 	size_t size = 0;
 	size_t i = 0;
-	print_usage(argv);
+	printf("Enter executable name: ");
 	while((i = getline(&buffer, &size, stdin)) > 0) {
 		buffer[i-1] = 0;
 		puts(buffer);
 		if (access(buffer, X_OK) == 0) {
 			// execute(buffer);
 			printf("executing %s\n", buffer);
+		} else {
+			print_usage(argv);
 		}
-		print_usage(argv);
+		printf("Enter executable name: ");
 		free(buffer);
 		buffer = NULL;
 		size = 0;
 	}
 	return 0;
+}
+
+void print_usage(char const *argv[]) {
+	printf("usage: ./%s [executable]\n", argv[0]);
 }
