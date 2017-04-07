41c09de2ea4375b8744ca4ef0b47c2d49f6c0168
acourtn2 <acourtn2@illinois.edu>
2017-04-06 23:36:57 -0500

    taskManager

--- taskManager.c
+++ taskManager.c
@@ -3,4 +3,50 @@
 
 void manageTask(node* head){
   if(head == NULL){return;}
+
+  node* tmp = head;
+  while(tmp != NULL){
+    if(tmp->taskNo == 0){ //no task assigned
+      
+    } else if(tmp->taskNo == 1){
+      handleTaskOne(tmp);
+    } else if(tmp->taskNo == 2){
+      handleTaskTwo(tmp);
+    } else if(tmp->taskNo == 3){
+      handleTaskThree(tmp);
+    } else if(tmp->taskNo == 4){
+      handleTaskFour(tmp);
+    } else if(tmp->taskNo == 5){
+      handleTaskFive(tmp);
+    } else if(tmp->taskNo == 6){
+      handleTaskSix(tmp);
+    } else {
+      fprintf(stderr, "taskmanager got a node with weird taskno %d\n", tmp->taskNo);
+    }
+
+
+    tmp = tmp->next;
+  }
+}
+
+void handleTaskOne(node* task){
+  //expects a message
+  
+  //sends 
+}
+
+void handleTaskTwo(node* task){
+
+}
+void handleTaskThree(node* task){
+
+}
+void handleTaskFour(node* task){
+
+}
+void handleTaskFive(node* task){
+
+}
+void handleTaskSix(node* task){
+
 }
--- taskManager.h
+++ taskManager.h
@@ -1,7 +1,39 @@
 #ifndef _TASK_MANAGER_H_
 #define _TASK_MANAGER_H_
 #include "node.h"
+
+#define READY "Ready for work."
+#define RECEIVEME "Do you receive me?"
+#define RECEIVED "Yes, I received you."
+#define RUNNING "Running."
+#define ASKRUNNING "Are you running?"
+#define ASKSTATUS "What is your status?"
+#define DONERUNNING "Finished running."
+#define ASKFILE "Send me the file:"
+
+/*implimentation details
+  every sent message will be headed with a 1byte "info" section
+  bit0:  valid
+  bit1:  undeclared as of yet
+  bit2:  EOF
+  bit3:  check/bool statement
+  bit4:  exec file
+  bit5:  data file
+  bit6:  status
+  bit7:  error
+
+
+
+
 void manageTask(node* head);
+
+
+void handleTaskOne(node* task);
+void handleTaskTwo(node* task);
+void handleTaskThree(node* task);
+void handleTaskFour(node* task);
+void handleTaskFive(node* task);
+void handleTaskSix(node* task);
 
 
 #endif
