4183ccf4978504532823ebbfd097f01dff9d4aeb
sstevenshang <sstevenshang@gmail.com>
2017-04-13 13:42:22 -0500

    some changes

--- scheduler.c
+++ scheduler.c
@@ -1,9 +1,17 @@
 #include <scheduler.h>
 
 void schedule_task(char* task) {
-
+    if (tasks == NULL)
+        tasks = queue_create();
+    queue_push(tasks, task);
+    size++;
 }
 
 void distribute_task(node* workers) {
 
+}
+
+void shutdown_scheduler() {
+    queue_destroy(tasks);
+    tasks = NULL;
 }
--- scheduler.h
+++ scheduler.h
@@ -2,5 +2,7 @@
 #include <queue.h>
 #include <node.h>
 
+queue* tasks;
+
 void schedule_task(char* task);
 void distribute_task(node* workers);
