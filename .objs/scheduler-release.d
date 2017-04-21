4557fe9f8e58710780422f945cb72c2ea9f5ada8
acourtn2 <acourtn2@illinois.edu>
2017-04-20 22:09:05 -0500

    q

--- .objs/scheduler-release.d
+++
@@ -1,7 +0,0 @@
-.objs/scheduler-release.o: scheduler.c scheduler.h queue.h node.h
-
-scheduler.h:
-
-queue.h:
-
-node.h:
--- taskManager.h
+++ taskManager.h
@@ -15,12 +15,12 @@
 #define ASKSTATUS "What is your status?"
 #define DONERUNNING "Finished running."
 #define ASKFILE "Send me the file:"
-#define iEOF 0x05
-#define iBool 0x09
-#define iExe 0x11
-#define iData 0x21
-#define iStatus 0x41
-#define iErr 0x81
+#define iEOF "\x05"
+#define iBool "\x09"
+#define iExe "\x11"
+#define iData "\x21"
+#define iStatus "\x41"
+#define iErr "\x81"
 
 /*implimentation details
   every sent message will be headed with a 1byte "info" section
