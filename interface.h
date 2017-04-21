b8f45e006cd3c29d581918c30445d5cdadc90924
Ip <ngai0408@gmail.com>
2017-04-21 00:14:53 -0500

    Add interface.h

---
+++ interface.h
@@ -0,0 +1,20 @@
+#ifndef _INTERFACE_H_
+#define _INTERFACE_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "node.h"
+#include "master.h"
+#include "worker.h"
+
+int wait_for_input();
+void print_usage();
+int setUpClient(char *addr, char *port);
+int cleanUpClient(int socket);
+int sendBinaryFile(int socket, char *name);
+ssize_t my_write(int socket, void *buffer, size_t count);
+void write_binary_data(FILE *f, int sockFd, char *buffer);
+
+#endif
